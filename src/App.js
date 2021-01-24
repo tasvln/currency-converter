b3843f3339c1d9327c1ac19e0e4b6078fa129280
Adebayo Temitope <adecancode@gmail.com>
2021-01-24 20:16:35 +0100

    +

--- src/App.js
+++ src/App.js
@@ -1,20 +1,83 @@
+import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import Converter from './components/Coverter'
 import Header from './components/Header'
 
 export default function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+  const [exchangeRate, setExchangeRate] = useState()
+  const [amount, setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
+
+  let toAmount, fromAmount
+  if (amountInFromCurrency) {
+    fromAmount = amount
+    toAmount = amount * exchangeRate
+  } else {
+    toAmount = amount
+    fromAmount = amount / exchangeRate
+  }
+
+  useEffect(() => {
+    axios.get(`https://api.exchangeratesapi.io/latest`)
+      .then(res => {
+        return res.data
+      })
+      .then(data => {
+        const firstCurrency = Object.keys(data.rates)[0]
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        setToCurrency(firstCurrency)
+        setExchangeRate(data.rates[firstCurrency])
+      })
+  }, [])
+
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      axios.get(`https://api.exchangeratesapi.io/latest?base=${fromCurrency}&symbols=${toCurrency}`)
+        .then(res => {
+          return res.data
+        })
+        .then(data => setExchangeRate(data.rates[toCurrency]))
+    }
+  }, [fromCurrency, toCurrency])
+
+  const handleFromAmountChange = (e) => {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true)
+  }
+
+  const handleToAmountChange = (e) => {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false)
+  }
+
   return (
     <div>
       <Header />
       <div className="mx-20 my-5 flex">
         <div className="w-1/3 w-fu bg-white justify-center align-center p-4 rounded-lg mx-auto">
           <div>
-            <span className="bg-gray-50 text-gray-500 text-sm font-semibold px-4 py-1 rounded-full">From</span>
-            <Converter />
+            <span className="bg-gray-50 text-gray-600 text-sm font-semibold px-4 py-1 rounded-full">From</span>
+            <Converter 
+              currencyOptions={currencyOptions}
+              selectedCurrency={fromCurrency}
+              Currency={e => setFromCurrency(e.target.value)}
+              onChangeAmount={handleFromAmountChange}
+              amount={fromAmount}
+            />
           </div>
           <div className="mt-6">
-            <span className="bg-gray-50 text-gray-500 text-sm font-semibold px-4 py-1 rounded-full">To</span>
-            <Converter />
+            <span className="bg-gray-50 text-gray-600 text-sm font-semibold px-4 py-1 rounded-full">To</span>
+            <Converter 
+              currencyOptions={currencyOptions}
+              selectedCurrency={toCurrency}
+              Currency={e => setToCurrency(e.target.value)}
+              onChangeAmount={handleToAmountChange}
+              amount={toAmount}
+            />
           </div>
         </div>
       </div>
--- src/components/Coverter.js
+++ src/components/Coverter.js
@@ -1,12 +1,29 @@
 import React from 'react'
 
-export default function Converter() {
+export default function Converter(props) {
+    const {
+        currencyOptions,
+        selectedCurrency,
+        Currency,
+        Amount,
+        amount
+    } = props
+
     return (
         <div className="mt-4 flex">
-            <input type="number" className="bg-gray-50 py-2 px-4 w-full rounded-lg mr-4" />
-            <select className="bg-gray-50 py-2 pl-4 pr-6 appearance-none rounded-lg">
-                <option>omo</option>
-            </select>
+            <input type="number" className="bg-gray-50 py-2 px-4 w-full rounded-lg mr-4" value={amount} onChange={Amount} />
+            <div className="relative flex">
+                <select className="bg-gray-50 py-2 pl-3 pr-6 appearance-none rounded-lg" value={selectedCurrency} onChange={Currency}>
+                    {currencyOptions.map(option => (
+                        <option key={option} value={option}>{option}</option>
+                    ))}
+                </select>
+                <div className="ml-3 absolute inset-y-0 right-0 flex items-center pointer-events-none">
+                    <svg class="h-5 w-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+                    </svg>
+                </div>
+            </div>
         </div>
     )
 }
