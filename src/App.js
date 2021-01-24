8d817f8ce269aa06782291855bcb662eeb23614a
Adebayo Temitope <adecancode@gmail.com>
2021-01-24 19:19:23 +0100

    +

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import Converter from './components/Coverter'
 import Header from './components/Header'
 
 export default function App() {
@@ -6,8 +7,15 @@ export default function App() {
     <div>
       <Header />
       <div className="mx-20 my-5 flex">
-        <div className="w-1/2 w-fu bg-white justify-center align-center p-4 rounded-lg mx-auto">
-          
+        <div className="w-1/3 w-fu bg-white justify-center align-center p-4 rounded-lg mx-auto">
+          <div>
+            <span className="bg-gray-50 text-gray-500 text-sm font-semibold px-4 py-1 rounded-full">From</span>
+            <Converter />
+          </div>
+          <div className="mt-6">
+            <span className="bg-gray-50 text-gray-500 text-sm font-semibold px-4 py-1 rounded-full">To</span>
+            <Converter />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Coverter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Converter() {
+    return (
+        <div className="mt-4 flex">
+            <input type="number" className="bg-gray-50 py-2 px-4 w-full rounded-lg mr-4" />
+            <select className="bg-gray-50 py-2 pl-4 pr-6 appearance-none rounded-lg">
+                <option>omo</option>
+            </select>
+        </div>
+    )
+}
